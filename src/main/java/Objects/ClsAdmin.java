8f03751ae18d954171b577dc84e37deac98f454a
Tribog <151857157+TribogL@users.noreply.github.com>
2025-11-15 18:57:17 -0500

    Refactor ClsAdmin to extend ClsPeople and add fields

--- src/main/java/Objects/ClsAdmin.java
+++ src/main/java/Objects/ClsAdmin.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Objects;
 
-/**
- *
- * @author MSI TIN
- */
-public class ClsAdmin {
-    
+
+public class ClsAdmin extends ClsPeople{
+    private String Range;
+
+    public ClsAdmin() {
+        this.Range = "";
+    }
+
+    public ClsAdmin(int IDDNI, String Name, String LastName, String Email, String Phone, String Password, String Project, boolean Permissions, int CantRequest, String Area, String Range) {
+        super(IDDNI, Name, LastName, Email, Phone, Password, Project, Permissions, CantRequest, Area);
+        
+        this.Range = Range;
+    }
+
+    public String getRange() {
+        return Range;
+    }
+
+    public void setRange(String Range) {
+        this.Range = Range;
+    }               
 }
+
